b936ce517cf4df041b8187b78cc429518bdb458e
ikkitonga <mcgaston@gmail.com>
2024-07-17 22:47:14 -0300

    se agrego componente home a app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import Home from './componentes/home/Home'
 import Navbar from './componentes/navbar/Navbar'
 
 function App() {
-  
+
   return (
-    <>
+    <section className='app'>
       <Navbar />
-    </>
+      <div>
+        <Home />
+      </div>
+
+    </section>
   )
 }
 
