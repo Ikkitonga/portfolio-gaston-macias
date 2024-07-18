85f55093cf74d5c02ad39ec4ec1017a28cdda75f
ikkitonga <mcgaston@gmail.com>
2024-07-17 22:48:21 -0300

    primeros pasos del componente home

---
+++ src/componentes/home/Home.jsx
@@ -0,0 +1,17 @@
+import "./home.css";
+
+const Home = () => {
+    return <section className="home__contenedor">
+        <div className="home__presentacion">
+            <h1>Hola, soy Gastón y este es mi Portfolio</h1>
+
+            <p className="home__parrafo">Desarrollador FRONTEND Y Tester QA</p>
+            <p className="home__parrafo">Ademas soy Técnico ELECTRONICO orientado a las telecomunicaciones</p>
+        </div>
+        <div className="home__vinculos">
+
+        </div>
+    </section>
+}
+
+export default Home;
