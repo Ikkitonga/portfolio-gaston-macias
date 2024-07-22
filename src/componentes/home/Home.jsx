5d7ebcb119d6db348597ebbcac9cce60058b407f
ikkitonga <mcgaston@gmail.com>
2024-07-22 14:51:57 -0300

    componente Home

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -3,10 +3,10 @@ import "./home.css";
 const Home = () => {
     return <section className="home__contenedor">
         <div className="home__presentacion">
-            <h1>Hola, soy Gastón y este es mi Portfolio</h1>
+            <h1>Hola, soy <span className="resalte">Gaston</span> y este es mi Portfolio</h1>
 
-            <p className="home__parrafo">Desarrollador FRONTEND Y Tester QA</p>
-            <p className="home__parrafo">Ademas soy Técnico ELECTRONICO orientado a las telecomunicaciones</p>
+            <p className="home__parrafo">Desarrollador <span className="resalte">FRONTEND</span> Y <span className="resalte">Tester QA</span></p>
+            <p className="home__parrafo">Ademas soy <span className="resalte">Técnico ELECTRONICO</span> orientado a las telecomunicaciones</p>
         </div>
         <div className="home__vinculos">
 
