300e6233938e90e37f3e2427c29eacd8185950e6
ikkitonga <mcgaston@gmail.com>
2024-07-22 14:35:51 -0300

    implementacion de background y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 
 import './App.css'
+import Background from './componentes/background/Background'
 import Home from './componentes/home/Home'
 import Navbar from './componentes/navbar/Navbar'
 
 function App() {
 
   return (
+    <Background imageUrl="https://static.vecteezy.com/system/resources/previews/009/392/527/non_2x/web-page-on-a-laptop-screen-a-mobile-phone-is-lying-a-cloud-a-cloud-service-or-an-application-modern-technological-background-isometry-conceptual-banner-of-web-technologies-vector.jpg">
     <section className='app'>
       <Navbar />
-      <div>
-        <Home />
-      </div>
+      
 
     </section>
+    </Background> 
   )
 }
 
