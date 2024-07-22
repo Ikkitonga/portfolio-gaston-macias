7008d324a58a76d6d458c5388f05e63959d79c77
ikkitonga <mcgaston@gmail.com>
2024-07-22 14:51:39 -0300

    implementacion de home y estilo de letra

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Background imageUrl="https://static.vecteezy.com/system/resources/previews/009/392/527/non_2x/web-page-on-a-laptop-screen-a-mobile-phone-is-lying-a-cloud-a-cloud-service-or-an-application-modern-technological-background-isometry-conceptual-banner-of-web-technologies-vector.jpg">
     <section className='app'>
       <Navbar />
-      
+      <Home />
 
     </section>
     </Background> 
