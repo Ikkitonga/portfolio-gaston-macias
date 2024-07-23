c13682bd266fe7128fe1f10a16620108ca3cbb82
ikkitonga <mcgaston@gmail.com>
2024-07-23 17:50:34 -0300

    se corrigio etiqueta de cierre de background

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 
 import './App.css'
 import Background from './componentes/background/Background'
+import Habilidades from './componentes/habilidades/Habilidades'
 import Home from './componentes/home/Home'
 import Navbar from './componentes/navbar/Navbar'
 
 function App() {
 
   return (
-    <Background imageUrl="https://static.vecteezy.com/system/resources/previews/009/392/527/non_2x/web-page-on-a-laptop-screen-a-mobile-phone-is-lying-a-cloud-a-cloud-service-or-an-application-modern-technological-background-isometry-conceptual-banner-of-web-technologies-vector.jpg">
-    <section className='app'>
-      <Navbar />
-      <Home />
-
-    </section>
-    </Background> 
+    <>
+      <Background imageUrl="https://static.vecteezy.com/system/resources/previews/009/392/527/non_2x/web-page-on-a-laptop-screen-a-mobile-phone-is-lying-a-cloud-a-cloud-service-or-an-application-modern-technological-background-isometry-conceptual-banner-of-web-technologies-vector.jpg" />
+      <section className='app'>
+        <Navbar />
+        <div>
+          <Home />
+        </div>
+        <div>
+          <Habilidades />
+        </div>
+      </section>
+    </>
   )
 }
 
