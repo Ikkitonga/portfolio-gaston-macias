af38f9849ae9f34fba4f42fc82e0475b88bc2af2
ikkitonga <mcgaston@gmail.com>
2024-07-26 17:21:14 -0300

    vinculos en el home y algunos estilos

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -2,14 +2,22 @@ import "./home.css";
 
 const Home = () => {
     return <section className="home__contenedor">
-        <div className="home__presentacion">
-            <h1>Hola, soy <span className="resalte">Gaston</span> y este es mi Portfolio</h1>
+        <div className="presentacion_vinculos">
+            <div className="home__presentacion">
+                <h1>Hola, soy <span className="resalte">Gaston</span> y este es mi Portfolio</h1>
 
-            <p className="home__parrafo">Desarrollador <span className="resalte">FRONTEND</span> Y <span className="resalte">Tester QA</span></p>
-            <p className="home__parrafo">Ademas soy <span className="resalte">Técnico ELECTRONICO</span> orientado a las telecomunicaciones</p>
-        </div>
-        <div className="home__vinculos">
+                <p className="home__parrafo">Desarrollador <span className="resalte">FRONTEND</span> Y <span className="resalte">Tester QA</span></p>
+                <p className="home__parrafo">Ademas soy <span className="resalte">Técnico ELECTRONICO</span> orientado a las telecomunicaciones</p>
+            </div>
 
+            <div className="home__vinculos">
+                <a href="https://github.com/Ikkitonga" target="_blank"><i className="fa-brands fa-square-github"></i></a>
+
+                <a href="https://www.linkedin.com/in/gastonezequielmacias" target="_blank"><i
+                    className="fa-brands fa-linkedin"></i></a>
+
+                <a href="./CURRICULUM/Gaston Ezequiel Macias.pdf" className="curriculum" target="_blank">CV para descarga</a>
+            </div>
         </div>
     </section>
 }
