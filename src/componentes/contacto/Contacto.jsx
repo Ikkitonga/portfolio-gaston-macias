5ab5eaace6db33a6ef6cfdd8a1e50e971b4f272a
ikkitonga <mcgaston@gmail.com>
2024-07-29 15:53:15 -0300

    componente contacto

---
+++ src/componentes/contacto/Contacto.jsx
@@ -0,0 +1,11 @@
+import "./contacto.css";
+
+
+const Contacto = () => {
+    return <section className="seccion__contacto" id="contacto">
+        <h2>CONTACTO</h2>
+    </section>
+}
+
+
+export default Contacto;
