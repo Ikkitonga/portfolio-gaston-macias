b5bb93fa647914bd20810527281de4c737d23975
ikkitonga <mcgaston@gmail.com>
2024-07-31 13:07:53 -0300

    agregado componente contacto

--- src/componentes/formularioContacto/FormularioContacto.jsx
+++ src/componentes/formularioContacto/FormularioContacto.jsx
@@ -1,13 +1,61 @@
 import "./formularioContacto.css";
+import React, { useState } from "react";
+
 
 const FormularioContacto = () => {
-    return <form className="formulario">
-        <div className="">
-            <label htmlFor="nombre">Nombre</label>
-            <input type="text" />
-            <label htmlFor="">Apellido</label>
-            <input type="text" />
-        </div>
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Formulario enviado:", formData);
+    };
+
+    return <form className="seccion__formulario" onSubmit={handleSubmit}>
+
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+        />
+
+        <label htmlFor="">Email:</label>
+        <input type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+        />
+
+
+        <label htmlFor="mensaje">Mensaje:</label>
+        <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+        />
+
+        <button type="submit">ENVIAR</button>
+
     </form>
 }
 
