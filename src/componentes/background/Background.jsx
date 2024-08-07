e10644ac36f8bc9b459e6d927febd050bfe71b89
ikkitonga <mcgaston@gmail.com>
2024-08-07 18:53:30 -0300

    cambios en comentarios

--- src/componentes/background/Background.jsx
+++ src/componentes/background/Background.jsx
@@ -2,9 +2,9 @@ import "./background.css";
 
 const Background = ({ imageUrl, children }) => {
 
-    // se realizan las siguientes constantes para cambiar el fondo de pantalla para la media query de moviles a partir de los 412px
+    // la constante isMobile es para cambiar el fondo de pantalla para la media query de moviles a partir de los 430px
 
-    const isMobile = window.innerWidth <= 412;
+    const isMobile = window.innerWidth <= 430;
     const mobileImageUrl = "https://i.pinimg.com/736x/62/c2/30/62c230e25c6239c33d2954641b9f5467.jpg"
 
     return <div className="background" style={{backgroundImage: `url(${isMobile ? mobileImageUrl : imageUrl})`}}>
